c09806760295f5ff039448931442d79ce960db8a
Laukpindang <ferdiansyahbandung3@gmail.com>
2025-04-06 20:58:42 +0700

    feat: add react-helmet to change document title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet'
+
 import { SidebarTrigger } from './ui/sidebar'
 import { HeaderUser } from './header-user'
 
@@ -7,15 +9,20 @@ interface Props {
 
 const Header = ({ title }: Props) => {
   return (
-    <header className='flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4'>
-      <div className='flex items-center gap-2'>
-        <SidebarTrigger className='-ml-1' />
-        <p>{title}</p>
-      </div>
-      <div className='flex flex-col items-end'>
-        <HeaderUser />
-      </div>
-    </header>
+    <>
+      <Helmet>
+        <title>SOWEGO | {title}</title>
+      </Helmet>
+      <header className='flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4'>
+        <div className='flex items-center gap-2'>
+          <SidebarTrigger className='-ml-1' />
+          <p>{title}</p>
+        </div>
+        <div className='flex flex-col items-end'>
+          <HeaderUser />
+        </div>
+      </header>
+    </>
   )
 }
 
